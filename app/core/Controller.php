f98b193aff7878a304a2557326bb0c54cfee81c8
RichardSoares <soares.richard@gmail.com>
2018-07-31 18:41:40 -0400

    Fixed array offset (0) when no $data passed to the View by Controller.

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -33,9 +33,11 @@ class Controller
         if(file_exists(VIEWS_PATH . $view . '.php'))
         {
             // Set each index of data to its named variable.
-            if( is_array($data[0]) ) {
-                foreach($data[0] as $key => $value) {
-                    $$key = $value;
+            if( count($data) > 0 ) {
+                if( is_array($data[0]) ) {
+                    foreach($data[0] as $key => $value) {
+                        $$key = $value;
+                    }
                 }
             }
             require_once VIEWS_PATH . $view . '.php';
